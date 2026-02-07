d2ab2a77e1fb0cd30a9e2d50edc14f4c59eb9b78
Ashwin Chinnaswamy <chin5657@wths.net>
2026-02-07 12:11:10 -0600

    constants

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,31 @@
+package frc.robot;
+public final class Constants {
+    public static final class DriveConstants{
+        public static final int LEFT_LEADER_ID = 1;
+        public static final int LEFT_FOLLOWER_ID = 2;
+        public static final int RIGHT_LEADER_ID = 3;
+        public static final int RIGHT_FOLLOWER_ID = 4;
+        public static final int DRIVE_MOTOR_CURRENT_LIMIT = 60;
+    
+    }
+    public static final class FuelConstants {
+        public static final int FEEDER_MOTOR_ID = 6;
+        public static final int INTAKE_LAUNCHER_MOTOR_ID = 5;
+        public static final double INTAKING_FEEDER_VOLTAGE = -12;
+        public static final double INTAKING_INTAKE_VOLTAGE = -10;
+        public static final double LAUNCHING_FEEDER_VOLTAGE = 9;
+        public static final double LAUNCHING_LAUNCHER_VOLTAGE = 10.6;
+        public static final double SPIN_UP_FEEDER_VOLTAGE = -6;
+        public static final double SPIN_UP_SECONDS = 1;
+    }
+    public static final class OperatorConstants{
+        public static final int DRIVER_CONTROLLER_PORT = 0;
+        public static final int OPERATOR_CONTROLLER_PORT = 1;
+        public static final double DRIVE_SCALING = 0.7;
+        public static final double ROTATION_SCALING = 0.8;
+        }
+
+    
+
+    
+}
--- src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
@@ -1,3 +1,32 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkbBase.PersistMode;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.DriveConstants.*;
+
+public class CANDriveSubsystem extends SubsystemBase {
+    private final SparkMax leftLeader;
+    private final SparkMax leftFollower;
+    private final SparkMax rightLeader;
+    private final SparkMax rightFollower;
+    private final DifferentialDrive drive;
+    public CANDriveSubsystem(){
+        leftLeader = new SparkMax(LEFT_LEADER_ID, MotorType.kBrushed);
+        leftFollower = new SparkMax(LEFT_FOLLOWER_ID, MotorType.kBrushed);
+        rightLeader = new SparkMax(RIGHT_LEADER_ID, MotorType.kBrushed);
+        rightFollower = new SparkMax(RIGHT_FOLLOWER_ID, MotorType.kBrushed);
+        
+        drive = new DifferentialDrive(leftLeader, rightLeader);
+        leftLeader.setCANTimeout(250);
+        leftFollower.setCANTimeout(250);
+        rightLeader.setCANTimeout(250);
+        rightFollower.setCANTimeout(250);
+    }
+    
+    
+}
